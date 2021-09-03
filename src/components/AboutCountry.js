5dce0f0ce350dce8e0130309260b4b0623ba13ae
Sid <sidchopp@gmail.com>
2021-09-03 11:21:03 -0600

    name of country by geolocation is rendered on UI

--- src/components/AboutCountry.js
+++ src/components/AboutCountry.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
 
 function AboutCountry() {
+
+  const [location, setLocation] = useState([]);
+
+  // Promisifying the Geolocation API
+  const getPosition = function () {
+    return new Promise(function (resolve, reject) {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+  };
+
+  const whereAmI = async function () {
+    const pos = await getPosition();
+    //console.log('value of my position:', pos);
+
+    const { latitude: lat, longitude: lng } = pos.coords;
+    //console.log('My position:', lat, lng);
+
+    const responseGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    if (!responseGeo.ok) throw new Error("Problem getting location data")
+    const dataGeo = await responseGeo.json();
+    //console.log('Response from dataGeo:', dataGeo);
+
+    const response = await fetch(`https://restcountries.eu/rest/v2/name/${dataGeo.country}?fullText=true`);
+    if (!response.ok) throw new Error("Problem getting country")
+    //console.log(response);
+    const data = await response.json();
+    console.log(data);
+    setLocation(data)
+
+
+  }
+
+  useEffect(() => {
+    whereAmI();
+  }, [])
+
+  const mapLocation = location.map((country) => {
+    return (
+      // console.log(country)
+      <div>{country.name}</div>
+    )
+
+  })
   return (
     <div>
-      My Country
+      {mapLocation}
     </div>
   )
 }

58a74524201221d85dccabcfe0452f72d5ce99b6
Sid <sidchopp@gmail.com>
2022-01-29 15:40:33 -0700

    Lazyloading of cards

--- src/components/AllCountriesCards.js
+++ src/components/AllCountriesCards.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card, Image, Label } from 'semantic-ui-react'
+import LazyLoad from 'react-lazyload';
 
 // CSS
 import '../App.css'
@@ -9,55 +10,57 @@ function AllCountriesCards({ allCountries }) {
   return (
     <div>
       <Card raised className='font ' style={{ padding: '25px', margin: '20px' }} >
-        <Card.Content>
-          <Label size='large' attached='top' >{allCountries.region}</Label>
-          <Card.Header className='uppercase' style={{ marginBottom: '5px' }} >
-            {allCountries.name}
-          </Card.Header>
-          <Card.Meta >
-            Capital:
-            {allCountries.capital}
-          </Card.Meta>
-          <Image rounded size='small' style={{ margin: '15px' }} src={allCountries.flags.png} wrapped />
-          <Card.Description>
-            {/* <div>
+        <LazyLoad height={1400} offset={[-200, 0]}>
+          <Card.Content>
+            <Label size='large' attached='top' >{allCountries.region}</Label>
+            <Card.Header className='uppercase' style={{ marginBottom: '5px' }} >
+              {allCountries.name}
+            </Card.Header>
+            <Card.Meta >
+              Capital:
+              {allCountries.capital}
+            </Card.Meta>
+            <Image rounded size='small' style={{ margin: '15px' }} src={allCountries.flags.png} wrapped />
+            <Card.Description>
+              {/* <div>
               <span>
                 <b>  Region:</b>
               </span>
               <span> {allCountries.region} </span>
             </div> */}
-            <div>
-              <span>
-                <b>  Sub-Region:</b>
-              </span>
-              <span>{[allCountries.subregion]}</span>
-            </div>
-            <div>
-              <span>
-                <b>  Area:</b>
-              </span>
-              <span>
-                {+(allCountries.area / 1000).toFixed(1)}K sq.km
-              </span>
               <div>
                 <span>
-                  <b> Language:</b>
+                  <b>  Sub-Region:</b>
+                </span>
+                <span>{[allCountries.subregion]}</span>
+              </div>
+              <div>
+                <span>
+                  <b>  Area:</b>
                 </span>
                 <span>
-                  {allCountries.languages[0].name}
+                  {+(allCountries.area / 1000).toFixed(1)}K sq.km
+                </span>
+                <div>
+                  <span>
+                    <b> Language:</b>
+                  </span>
+                  <span>
+                    {allCountries.languages[0].name}
+                  </span>
+                </div>
+              </div>
+              <div>
+                <span>
+                  <b> Population:</b>
+                </span>
+                <span>
+                  {+(allCountries.population / 1000000).toFixed(4)} M
                 </span>
               </div>
-            </div>
-            <div>
-              <span>
-                <b> Population:</b>
-              </span>
-              <span>
-                {+(allCountries.population / 1000000).toFixed(4)} M
-              </span>
-            </div>
-          </Card.Description>
-        </Card.Content>
+            </Card.Description>
+          </Card.Content>
+        </LazyLoad>
       </Card >
     </div>
   )
